7a367dc7f0d0bca86e781f546098ca67301067cd
hideki kuno <kunohi@firstserver.co.jp>
2018-07-24 08:56:53 +0900

    サンプルプログラムの更新

--- src/examples/koch.scm
+++ src/examples/koch.scm
@@ -3,24 +3,23 @@
 ;
 ; hidekuno@gmail.com
 ;
-
-(define kcos (cs 60))
-(define ksin (sn 60))
-
-(define koch (lambda (x0 y0 x1 y1 c)
-  (if (> c 1)
-      (let (
-            (xa (/ (+ (* x0 2) x1) 3))
-            (ya (/ (+ (* y0 2) y1) 3))
-            (xb (/ (+ (* x1 2) x0) 3))
-            (yb (/ (+ (* y1 2) y0) 3)))
-        (let ((yc (+ ya (+ (* (- xb xa) ksin) (* (- yb ya) kcos))))
-              (xc (+ xa (- (* (- xb xa) kcos) (* (- yb ya) ksin)))))
-        (koch x0 y0 xa  ya (- c 1))
-        (koch xa ya xc  yc (- c 1))
-        (koch xc yc xb  yb (- c 1))
-        (koch xb yb x1  y1 (- c 1))))
-      (draw_line x0 y0 x1 y1))))
+(define koch 
+  (lambda (x0 y0 x1 y1 c)
+    (let ((kcos (cs 60))
+          (ksin (sn 60)))
+      (if (> c 1)
+          (let (
+                (xa (/ (+ (* x0 2) x1) 3))
+                (ya (/ (+ (* y0 2) y1) 3))
+                (xb (/ (+ (* x1 2) x0) 3))
+                (yb (/ (+ (* y1 2) y0) 3)))
+            (let ((yc (+ ya (+ (* (- xb xa) ksin) (* (- yb ya) kcos))))
+                  (xc (+ xa (- (* (- xb xa) kcos) (* (- yb ya) ksin)))))
+              (koch x0 y0 xa  ya (- c 1))
+              (koch xa ya xc  yc (- c 1))
+              (koch xc yc xb  yb (- c 1))
+              (koch xb yb x1  y1 (- c 1))))
+          (draw_line x0 y0 x1 y1)))))
 
 (koch 259 0 34 390 4)
 (koch 34 390 483 390 4)
