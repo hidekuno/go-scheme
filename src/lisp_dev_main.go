023d9efe9212b19bce0aaad2fec7cff18d2dd047
hidekuno <hidekuno@gmail.com>
2018-08-24 17:16:53 +0900

    ファイル名の見直し

--- src/lisp_dev_main.go
+++ src/lisp_dev_main.go
@@ -7,49 +7,14 @@
 package main
 
 import (
-	"fmt"
 	"runtime"
-	"time"
+	"scheme"
 )
-
-func buildGoFunc() {
-	specialFuncTbl["go-append"] = func(env *SimpleEnv, v []Expression) (Expression, error) {
-		finish := make(chan bool, 2)
-		var result []Expression
-		var exp1 Expression
-		var exp2 Expression
-
-		go func() {
-			t0 := time.Now()
-			exp1, _ = eval(v[0], env)
-			t1 := time.Now()
-			fmt.Println("go-1", t1.Sub(t0))
-			finish <- true
-		}()
-		go func() {
-			t0 := time.Now()
-			exp2, _ = eval(v[1], env)
-			t1 := time.Now()
-			fmt.Println("go-2", t1.Sub(t0))
-			finish <- true
-		}()
-		<-finish
-		<-finish
-		result = append(result, (exp1.(*List)).Value...)
-		result = append(result, (exp2.(*List)).Value...)
-		return NewList(result), nil
-	}
-	specialFuncTbl["test-list"] = func(env *SimpleEnv, v []Expression) (Expression, error) {
-		time.Sleep(5 * time.Second)
-		var result []Expression
-		return NewList(result), nil
-	}
-}
 
 // Main
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	buildFunc()
-	buildGoFunc()
-	doInteractive()
+	scheme.BuildFunc()
+	scheme.BuildGoFunc()
+	scheme.DoInteractive()
 }
---
+++ src/scheme/lisp_dev.go
@@ -0,0 +1,46 @@
+/*
+   Go lang 3rd study program.
+   This is prototype program mini scheme subset.
+
+   hidekuno@gmail.com
+*/
+package scheme
+
+import (
+	"fmt"
+	"time"
+)
+
+func BuildGoFunc() {
+	specialFuncTbl["go-append"] = func(env *SimpleEnv, v []Expression) (Expression, error) {
+		finish := make(chan bool, 2)
+		var result []Expression
+		var exp1 Expression
+		var exp2 Expression
+
+		go func() {
+			t0 := time.Now()
+			exp1, _ = eval(v[0], env)
+			t1 := time.Now()
+			fmt.Println("go-1", t1.Sub(t0))
+			finish <- true
+		}()
+		go func() {
+			t0 := time.Now()
+			exp2, _ = eval(v[1], env)
+			t1 := time.Now()
+			fmt.Println("go-2", t1.Sub(t0))
+			finish <- true
+		}()
+		<-finish
+		<-finish
+		result = append(result, (exp1.(*List)).Value...)
+		result = append(result, (exp2.(*List)).Value...)
+		return NewList(result), nil
+	}
+	specialFuncTbl["test-list"] = func(env *SimpleEnv, v []Expression) (Expression, error) {
+		time.Sleep(5 * time.Second)
+		var result []Expression
+		return NewList(result), nil
+	}
+}
