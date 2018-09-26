8984a44f14ec78b10bb793ccce8ee6c223eecff6
hideki kuno <kunohi@firstserver.co.jp>
2018-09-26 15:59:01 +0900

    Web I/Fに対応(1 step)

---
+++ src/lisp_www_main.go
@@ -0,0 +1,46 @@
+/*
+   Go lang 3rd study program.
+   This is prototype program mini scheme subset.
+
+   hidekuno@gmail.com
+*/
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"scheme"
+)
+
+func doLisp(w http.ResponseWriter, r *http.Request) {
+	log.Print(r.URL)
+
+	bufbody := new(bytes.Buffer)
+	bufbody.ReadFrom(r.Body)
+
+	e, err := scheme.DoCoreLogic(bufbody.String(), rootEnv)
+	if err != nil {
+		fmt.Fprintln(w, err.Error())
+	} else {
+		e.Fprint(w)
+		fmt.Fprintln(w)
+	}
+}
+
+var (
+	rootEnv *scheme.SimpleEnv
+)
+
+// Main
+func main() {
+	scheme.BuildFunc()
+	rootEnv = scheme.NewSimpleEnv(nil, nil)
+
+	http.HandleFunc("/", doLisp)
+
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
