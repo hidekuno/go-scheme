dabf76dee8b40c43da3f18ac1e9f68fad23d60b1
hidekuno <hidekuno@gmail.com>
2019-01-15 16:37:49 +0900

    パッケージ構成見直し

--- src/lisp_dev_main.go
+++ src/lisp_dev_main.go
@@ -6,15 +6,26 @@
 */
 package main
 
+/*
+ex.1
+(time (append (list (let loop ((i 0)) (if (<= 300000 i) i (loop (+ 1 i))))) (list (let loop ((i 0)) (if (<= 300000 i) i (loop (+ 1 i)))))))
+(time (go-append (list (let loop ((i 0)) (if (<= 300000 i) i (loop (+ 1 i))))) (list (let loop ((i 0)) (if (<= 300000 i) i (loop (+ 1 i)))))))
+
+ex.2
+(time (append (perm (iota 8) 8)(perm (iota 8) 8)))
+(time (go-append (perm (iota 8) 8)(perm (iota 8) 8)))
+*/
+
 import (
 	"runtime"
 	"scheme"
+	"scheme/experiment"
 )
 
 // Main
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	scheme.BuildFunc()
-	scheme.BuildGoFunc()
+	experiment.BuildGoFunc()
 	scheme.DoInteractive()
 }
