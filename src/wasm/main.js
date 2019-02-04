5af2820e5a2d2264ec33158b9616ebf145dc23b9
hidekuno <hidekuno@gmail.com>
2019-02-04 13:14:56 +0900

    WebSocketが動いた

--- src/wasm/main.js
+++
@@ -1,7 +0,0 @@
-(() => {
-    var url = new URL(location.href);
-    var socket = new WebSocket(`ws//${url.host}/socket`);
-    socket.onmessage = e => {sExpression.value = JSON.parse(e.data);};
-    socket.onerror = e => console.log("[ONERROR]", e);
-    socket.onclose = e => console.log("[ONCLOSE]", e);
-})();
