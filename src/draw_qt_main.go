250a193a5d96fda3c25ffe93cee8556c75a9e79f
hidekuno <hidekuno@gmail.com>
2019-03-01 13:08:25 +0900

    昔作ったQtの練習プログラムを追加

---
+++ src/draw_qt_main.go
@@ -0,0 +1,18 @@
+/*
+   Go lang 3rd study program.
+   This is prototype program mini scheme subset.
+
+   hidekuno@gmail.com
+*/
+package main
+
+import (
+	"draw"
+	"github.com/therecipe/qt/widgets"
+)
+
+// Main
+func main() {
+	draw.BuildQtApp("draw-demo")
+	widgets.QApplication_Exec()
+}
