99ada5c137b1c2692cfc9b7eeafbb5e7714a926f
hidekuno <hidekuno@gmail.com>
2019-10-29 15:36:57 +0900

    schemeサンプルプログラムの整理

--- samples/sicp-draw-line.scm
+++
@@ -1,8 +0,0 @@
-;
-; this is a draw-line for sicp
-; https://github.com/hidekuno/rust-elisp/tree/master/glisp/samples
-;
-; hidekuno@gmail.com
-;
-(define draw-line-vect (lambda (p1 p2) (draw-line (* (xcor-vect p1) 718)(* (ycor-vect p1) 558)(* (xcor-vect p2) 718)(* (ycor-vect p2) 558))))
-
